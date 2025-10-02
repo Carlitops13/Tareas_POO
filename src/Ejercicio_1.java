993ebc40f4da4bbc3a675ac38d1121b6faacdb53
Carlos <carlos.puente2626@gmail.com>
2025-10-01 22:28:55 -0500

    Commit inicial

---
+++ src/Ejercicio_1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+ class productoPlastico{
+    String nombre;
+    double precio;
+    int stock;
+    productoPlastico(String nombre, double precio, int stock){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.stock=stock;
+    }
+     public double precioStock() {
+
+        return this.precio * this.stock;
+     }
+
+}
+public class Ejercicio_1{
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        productoPlastico[] almacen = new productoPlastico[5];
+        System.out.println("Registro de Productos de plástico");
+
+         for  (int i = 0; i < almacen.length; i++) {
+             System.out.println("\n---Registrando productos N°:  "+ (i+1)+ "---");
+             System.out.println("Ingrese el nombre del  producto ");
+             String nombre = lector.nextLine();
+             System.out.println("Ingrese el precio del producto ");
+             double precio = lector.nextDouble();
+             System.out.println("Ingrese stock del producto ");
+             int stock = lector.nextInt();
+             lector.nextLine();
+             almacen[i] = new productoPlastico(nombre, precio, stock);
+
+         }
+
+         double valorTotal=0.0;
+         for (productoPlastico producto: almacen){
+             valorTotal+= producto.precioStock();
+         }
+         System.out.println("\n--- El valor total de stock de  los productos es: "+ valorTotal);
+         System.out.println("Inventario");
+
+    }
+}
---
+++ src/Ejercicio_2.java
@@ -0,0 +1,91 @@
+import java.nio.channels.FileChannel;
+import java.util.Scanner;
+
+class registroClientes {
+    String nombres;
+    String apellidos;
+    String cedula;
+    String telefono;
+    String mail;
+   registroClientes (String nombres, String apellidos, String cedula, String telefono, String mail){
+       this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.mail= mail;
+   }
+public void MostrarDatos(){
+    System.out.println("--- Ficha del Cliente ---");
+    System.out.println("Nombres: " + this.nombres);
+    System.out.println("Apellidos: " + this.apellidos);
+    System.out.println("Cédula: " + this.cedula);
+    System.out.println("Teléfono: " + this.telefono);
+    System.out.println("Mail: " + this.mail);
+    System.out.println("-------------------------");
+}
+}
+
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        registroClientes[] clientes = new  registroClientes[2];
+        System.out.println("\n---REGISTRO DE CLIENTES---");
+        for (int i = 0; i < clientes.length; i++) {
+            System.out.println("\n---Registrando cliente N°: " + (i + 1) + "---");
+            String nombres;
+            String apellidos;
+            String cedula;
+            String telefono;
+            String mail;
+            do {
+                System.out.println("Ingrese sus nombres: ");
+                nombres = lector.nextLine();
+                if (nombres.isEmpty()) {
+                    System.out.println("Error! No ha ingresado sus nombres, inténtelo de nuevo ");
+
+                }
+            } while (nombres.isEmpty());
+            do {
+                System.out.println("Ingrese sus apellidos: ");
+                apellidos = lector.nextLine();
+                if (apellidos.isEmpty()) {
+                    System.out.println("Error! No ha ingresado sus apellidos, inténtelo de nuevo ");
+
+                }
+            } while (apellidos.isEmpty());
+            do {
+                System.out.println("Ingrese su cédula: ");
+                cedula = lector.nextLine();
+                if (cedula.isEmpty()) {
+                    System.out.println("Error! No ha ingresado su cédula, inténtelo de nuevo ");
+
+                }
+            } while (cedula.isEmpty());
+            do {
+                System.out.println("Ingrese su teléfono: ");
+                telefono = lector.nextLine();
+                if (telefono.isEmpty()) {
+                    System.out.println("Error! No ha ingresado su teléfono, inténtelo de nuevo ");
+
+                }
+            } while (telefono.isEmpty());
+            do {
+                System.out.println("Ingrese su e-mail: ");
+                mail = lector.nextLine();
+                if (mail.isEmpty() || !mail.contains("@")) {
+                    System.out.println("Error! No ha ingresado su email y debe incluir un @, inténtelo de nuevo ");
+                }
+            } while (mail.isEmpty());
+
+            clientes[i]= new registroClientes(nombres, apellidos, cedula, telefono, mail);
+            System.out.println("\n---Cliente  N°: "+ (i+1)+ " registrado con éxito---");
+        }
+        System.out.println("\n---Usuarios Registrados---");
+        for (registroClientes cliente: clientes ){
+            cliente.MostrarDatos();
+        }
+
+
+
+    }
+}
