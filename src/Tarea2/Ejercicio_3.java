e2444bb20c139b891385bef83903c1e7bc9a92e0
Carlos <carlos.puente2626@gmail.com>
2025-10-04 23:59:36 -0500

    Se agregó el ejercicio3 en Tarea2

--- src/Tarea2/Ejercicio_3.java
+++ src/Tarea2/Ejercicio_3.java
@@ -1,4 +1,17 @@
 package Tarea2;
 
+import java.util.Scanner;
+
 public class Ejercicio_3 {
+    public static void main(String[] args) {
+        Scanner ingreso = new Scanner(System.in);
+        System.out.print("Ingrese el número para sacar su tabla de multiplicar: ");
+        int numero = ingreso.nextInt();
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print(numero + " X " + i + " = " + (numero * i)+ "\n");
+        }
+
+    }
+
 }
