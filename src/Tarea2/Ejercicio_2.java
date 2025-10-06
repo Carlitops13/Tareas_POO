bafa93ab0549eff6a1e76af571ec073d14e54a2c
Carlos <carlos.puente2626@gmail.com>
2025-10-05 21:17:48 -0500

    Tarea2 Finalizada

--- src/Tarea2/Ejercicio_2.java
+++ src/Tarea2/Ejercicio_2.java
@@ -4,24 +4,19 @@ import java.util.Scanner;
 
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner lector = new Scanner(System.in);
         System.out.print("Cuantos números va a ingresar?: ");
-        int numeros= sc.nextInt();
+        int numeros= lector.nextInt();
         int contadorPares=0;
 
         for (int i = 1; i <= numeros; i++) {
             System.out.println("Ingresa el número "+ (i)+ ":");
-            int num= sc.nextInt();
+            int num= lector.nextInt();
             if (num%2==0) {
                 contadorPares++;
             }
 
         }
         System.out.println("Se ingresaron " + contadorPares + " números pares");
-
-
-
-
     }
 }
